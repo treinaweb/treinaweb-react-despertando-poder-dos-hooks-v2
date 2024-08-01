78335023cb5c79dcc57c024ca96811c347f9aa09
Wesley Gado <wesleygado@gmail.com>
2024-08-01 18:00:09 -0300

    Aula 05 - useRef

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,17 +1,25 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, createRef } from 'react'
 import './App.css'
 
 function Counter() {
-  const [contador, setContador] = useState(-1);
+  const [contador, setContador] = useState(0);
   const [title, setTitle] = useState('');
+  const contadorRef = useRef(0);
+  const input = createRef();
 
   useEffect(() => {
-    setContador(contador => contador + 1);
-  }, [title]);
+    setInterval(() => {
+      console.log(contadorRef.current);
+    }, 2000)
+  }, []);
+
+  useEffect(() => {
+    contadorRef.current = contador;
+  }, [contador]);
 
   return (
     <div>
-      <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+      <input ref={input} type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
       <button onClick={() => setContador(contador + 1)}>Contador: {contador}</button>
     </div>
   )
